5329e1a303c9b35f6fd65abd0cd8844a86f0535c
Kevin Njeru <knjeru@gmail.com>
2016-02-04 12:17:39 -0700

    Initial Functionality Achieved

--- src/js/trackProf.js
+++ src/js/trackProf.js
@@ -19,12 +19,16 @@ $(document).ready(function() {
     }
   });
 
-  $('#trackUserProf').show('slow', 'swing');
+  $('#trackUserProf').show();
 
   if (JSON.parse(localStorage.getItem("user")) === null) {
     $('#trackUserProf').empty();
-    $("#trackUserProf").append('<p class="gUP warning">Please fill out your profile to use our amazing calorie counter!</p>');
+    $("#trackUserProf").addClass('animated lightSpeedIn').append('<p class="gUP warning">Please fill out your profile to use our amazing calorie counter!</p>');
   } else {
+    // position form
+    $('html, body').animate({
+      scrollTop: $(".formfit").offset().top
+    }, 1000);
 
     //get the items from local storage for use on this page
     var currUser = JSON.parse(localStorage.getItem("user"));
@@ -33,17 +37,13 @@ $(document).ready(function() {
 
     // append the reccommended amount of calories to the document
     if (calsCON === undefined) {
-      $('#foodItem').append('<p class="gUP">Good Day, <strong>'+currUser.firstName+'</strong>!<br> What have you eaten today?</p>');
-      $('#calsConsumed').append('<p class="gUP">Enter a food item to start tracking calories</p>');
+      $('#foodItem').addClass('animated fadeIn').append('<p class="gUP">Good Day, <strong>'+currUser.firstName+'</strong>!<br> What have you eaten today?</p>');
+      $('#calsConsumed').addClass('animated fadeIn').append('<p class="gUP">Enter a food item to start tracking calories</p>');
     }
 
-    $("#calsRemain").append('<p class="gUP"><strong>' + recCals + '</strong></p>');
+    $("#calsRemain").addClass('animated fadeIn').append('<p class="gUP">You have <strong>' + recCals + '</strong> remaining!</p>');
 
   }
-
-  $('html, body').animate({
-    scrollTop: $("#water").offset().top
-  }, 1000);
 
   //pull API info for different foods
 
@@ -73,69 +73,68 @@ $(document).ready(function() {
             calsCON = calsFromFood.reduce(function(previousVal, currentVal) {
               return previousVal + currentVal;
             });
+
+            // call function to update the rec and current calories
+
             updateCalInfo();
 
             // show user the food item they just added
-            $('#foodItem').append('<p class="gUP">I hope that the <strong>' + capitalizeFirstLetter(baseFood) + '</strong> you had tasted good going down because it added <strong>' + calories + '</strong> calories to your body!</p>');
+            $('#foodItem').addClass('animated bounceIn').append("<p class='gUP'>I hope that the <strong>" + capitalizeFirstLetter(baseFood) + "</strong> you had tasted good going down because you've added <strong>" + calories + "</strong> calories to your body!</p>");
           }
         });
       },
-
+      // check for errors if the food is not in the database and
+      // append message to dom
       error: function(err) {
         console.log(err);
-        $('#foodItem').append('<p class="gUP">Sorry it appears as if our kitchen does not stock <strong>' + capitalizeFirstLetter(baseFood) + '</strong>. Maybe you should try selecting a real food next time?</p>');
+        $('#foodItem').addClass('animated bounceIn').append('<p class="gUP">Sorry it appears as if our kitchen does not stock <strong>' + capitalizeFirstLetter(baseFood) + '</strong>. Maybe you should try selecting a real food next time?</p>');
 
       }
     });
 
-    // clear input value
+    // clear input values
     $('input').val('');
     $('#foodItem').empty();
 
-    $('html, body').animate({
-      scrollTop: $("#water").offset().top
-    }, 1000);
+    // $('html, body').animate({
+    //   scrollTop: $("#water").offset().top
+    // }, 1000);
   });
 
   function updateCalInfo() {
 
     calsREM = Math.floor(recCals - calsCON);
     $('#calsConsumed').empty();
-    $('#calsConsumed').append('<p class="gUP"><strong>' + calsCON + '</strong></p>');
+    $('#calsConsumed').addClass('animated fadeIn').append("<p class='gUP'>You've consumed <strong>" + calsCON + "</strong> calories today.</p>");
     $("#calsRemain").empty();
-    $("#calsRemain").append('<p class="gUP"><strong>' + calsREM + '</strong></p>');
+    $("#calsRemain").addClass('animated bounceIn').append('<p class="gUP">You have <strong>' + calsREM + '</strong> remaining!</strong></p>');
   }
 
 
-  // append some goodies for water
+  // Append some goodies for water
 
   if (currWater === 0) {
-      $('#water').append("<p class='gUP'>Oh no! It looks like you haven't hydrated yet today! Quick take a sip before you wither away!</p>");
+      $('#water').addClass('animated fadeIn').append("<p class='gUP'>Oh no! It looks like you haven't hydrated yet today! Quick take a sip before you wither away!</p>");
   }
 
   var remWater = 0;
 
   $('#add').on('click', function() {
-    $('html, body').animate({
-      scrollTop: $("#water").offset().top
-    }, 1000);
     currWater = Math.floor(currWater + 8);
     remWater = Math.floor(64 - currWater);
     $('#water').empty();
     if (currWater === 8) {
-      $('#water').append("<p class='gUP'>Phew! You're up to " + currWater + " ounces of water now, but keep chugging!</p>");
-      $('#water').append("<p class='gUP'>Only " + remWater + " ounces to go!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='gUP'>Phew! You're up to <strong>" + currWater + "</strong> ounces of water now, but keep chugging!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='gUP'>Only <strong>" + remWater + "</strong> ounces to go!</p>");
     }
     else if (currWater < 64) {
-      $('#water').append("<p class='gUP'>You're up to " + currWater + " ounces of water!</p>");
-      $('#water').append("<p class='gUP'>Only " + remWater + " ounces to go!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='gUP'>You're up to <strong>" + currWater + "</strong> ounces of water!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='gUP'>Only <strong>" + remWater + "</strong> ounces to go!</p>");
     } else {
-      $('#water').append("<p class='gUP'>You're up to " + currWater + " ounces of water!</p>");
-      $('#water').append("<p class='appendedText'>You've reached your goal, but keeping going!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='gUP'>You're up to <strong>" + currWater + "</strong> ounces of water!</p>");
+      $('#water').addClass('animated bounceIn').append("<p class='appendedText'>You've reached your goal, but keeping going!</p>");
     }
 
   });
-
-
 
 });
